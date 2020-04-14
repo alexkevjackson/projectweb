514c93d2c07e9d6f1049a1b69eaac49d177aaeb5
Alexander Kevin Jackson <alexanderkevjackson@gmail.com>
2020-04-14 01:27:19 -0500

    Add templates, basic register form

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='templates')
 
-@app.route('/user/<username>')
-def index(username):
-    return render_template('index.html', username=username)
+@app.route('/register')
+def index():
+    return render_template('/auth/register.html')
 
 @app.route('/')
 def hello_world():
-    render_template
-    return 'Hello, World!'
+    return render_template('index.html')
