3e3f5f3d14bf0fd892719baa55312d2b31464c2c
Alexander Kevin Jackson <alexanderkevjackson@gmail.com>
2020-04-14 00:38:34 -0500

    Initialize template directory and provide example usage.

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/user/<username>')
+def index(username):
+    return render_template('index.html', username=username)
 
 @app.route('/')
 def hello_world():
+    render_template
     return 'Hello, World!'
